f9ec9a3a2a37bde53e33caa3f869fd9a5ed3f2b3
Sriram Sami <frizensami@gmail.com>
2016-11-12 19:59:09 +0800

    Removed MPI_BARRIERs, downloaded more results

--- code/SETL_par.c
+++ code/SETL_par.c
@@ -108,13 +108,7 @@ int main( int argc, char** argv)
         printSquareMatrix(curW, size + 2);
 #endif
 
-        //printf("Effective size: %d. Size with padding: %d\n", size, size+2);
-        //printf("Each processor must effectively take: %d rows", size / numprocs);
-       //printf("But with padding in front and behind, each must take: %d rows\n", size / numprocs + 2);
 
-
-    } else {
-        //printf("===> Not a root process, rank: %d out of %d\n", rank, numprocs);
     }
 
 
@@ -135,8 +129,6 @@ int main( int argc, char** argv)
         - each processor evolves the rows it has
         - each processors sends back 1) The patterns found + 2) The evolved rows
     */
-
-
 
 
     /*************************
@@ -205,8 +197,6 @@ int main( int argc, char** argv)
     int rowToSendEnd = startRow + numRowsPerProcessor - 1;
     int elementsToSend = (rowToSendEnd - rowToSendStart + 1) * (size + 2);
 
-    MPI_Barrier(MPI_COMM_WORLD);
-
     // Search for patterns in these rows
     // Increment the world pointers by the required amounts
     // size + 2 == number of elements in 1 row
@@ -235,8 +225,6 @@ int main( int argc, char** argv)
         evolveWorldNonSquare(curW, startRow, nextW, startRow + numRowsPerProcessor - 1, size);
         //printf("Rank %d done evolving world: World = \n", rank);
 
-        MPI_Barrier(MPI_COMM_WORLD);
-
         // After evolving - send your version of the world back to master
         // Each one sends their start row till end row
         // For all processors - we need to send the same amount of data
@@ -244,38 +232,20 @@ int main( int argc, char** argv)
 
         // Put into curW[1] because we want to avoid the 0th row
         MPI_Allgather(nextW[rowToSendStart], elementsToSend, MPI_CHAR, curW[1], elementsToSend, MPI_CHAR, MPI_COMM_WORLD);
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
     }
 
     for (int proc = 1; proc < numprocs; proc++) {
         if (rank == proc) {
             // If we are the process to be sending the data:
-
-
-            //printf("Rank %d starting to evolve world at address: %p\n", rank, nextWRowStart);
-            //evolveWorldNonSquare(curW, startRow, nextW, startRow + numRowsPerProcessor - 1, size);
-            //printf("Rank %d printing final list: \n", rank);
-            //printList(list);
-
-            //printf("Rank %d converting list to array, list: \n", rank);
             int* matchArray = matchlistToArray(list);
-            //printMatchArray(matchArray, list->nItem);
-
             int matchArraySize = list->nItem;
-
-            //printf("Rank %d sending\n", rank);
             MPI_Send(&matchArraySize, 1, MPI_INT, ROOT_PROCESS, tag, MPI_COMM_WORLD);
 
+            // IF there are matches to be sent
             if (matchArraySize > 0) {
                 int numberOfInts = matchArraySize * 4;
                 MPI_Send(matchArray, numberOfInts, MPI_INT, ROOT_PROCESS, tag, MPI_COMM_WORLD);
             }
-
-            //evolveWorld(curW, nextW, size);
-            //printf("Rank %d done evolving world expected: World = \n", rank);
-            //printSquareMatrix(nextW, size+2);
 
         } else if (rank == ROOT_PROCESS) {
             // If we are the root and we should be receiving
@@ -283,31 +253,23 @@ int main( int argc, char** argv)
             int matchArraySize = 0;
             MPI_Status status;
 
-            //printf("Root receiving from rank %d\n", proc);
+            // Get the match array's size
             MPI_Recv(&matchArraySize, 1, MPI_INT, proc, tag, MPI_COMM_WORLD, &status);
-            //printf("Root completed match array size reception from rank %d\n", proc);
 
             if (matchArraySize > 0) {
                 int numberOfInts = matchArraySize * 4;
                 int* matchArrayRecv = malloc(numberOfInts * sizeof(int));
 
-                //printf("Match array recv: %p\n", matchArrayRecv);
-
-                //printf("Root receiving <<MATCHARRAY>> from rank %d\n", proc);
+                // Receive the entire match array
                 MPI_Recv(matchArrayRecv, numberOfInts, MPI_INT, proc, tag, MPI_COMM_WORLD, &status);
-                //printf("Root completed match array ACTUAL reception from rank %d\n", proc);
 
                 for (int i = 0; i < numberOfInts; i+=4) {
-                    //printf("match array recv: %p\n", matchArrayRecv);
-                    //printf("i = %d, num = %d\n", i, numberOfInts);
-                    //printf("Iter: %d", matchArrayRecv[i]);
-                    //printf(" Row: %d", matchArrayRecv[i+1]);
-                    //printf(" Column: %d", matchArrayRecv[i+2]);
-                    //printf(" Pattern: %d\n", matchArrayRecv[i+3]);
+                    // Insert every match into the match list
                     insertEnd(list, matchArrayRecv[i], matchArrayRecv[i+1], matchArrayRecv[i+2], matchArrayRecv[i+3]);
 
                 }
 
+                // Free the memory that was used
                 free( matchArrayRecv );
             }
         }
@@ -321,18 +283,24 @@ int main( int argc, char** argv)
 
     if (rank == ROOT_PROCESS) {
 
-        //printf("\nFINAL LIST: \n");
-        // Prints the match list
-        //printList( list );
-
         MATCH* matches = matchlistToMatchArray(list);
 
-        //printf("Printing match struct array: \n");
-        //printMatchStructArray(matches, list->nItem);
+        long long beforesort = wallClockTime();
 
-        //printf("Sorting match struct array: \n");
+        // Sort the match array in order
         qsort(matches, list->nItem, sizeof(MATCH), matchSortFunc);
+
+        long long aftersort = wallClockTime();
+        printf("Parallel SETL sort took %1.2f seconds\n",
+            ((float)(aftersort - beforesort))/1000000000);
+
+        long long beforeprint = wallClockTime();
         printMatchStructArray(matches, list->nItem);
+        long long afterprint = wallClockTime();
+
+
+        printf("Parallel SETL print took %1.2f seconds\n",
+            ((float)(afterprint - beforeprint))/1000000000);
 
         //Stop timer
         after = wallClockTime();
--- code/copy.sh
+++ code/copy.sh
@@ -1,1 +1,1 @@
-scp -r . e0002863@nus.nscc.sg:~/3210-as2
+scp -r . e0002863@nus.nscc.sg:~/3210-as2/code
